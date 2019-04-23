0254565c7612fa2108be2d23735c78fd380c3e03
Jesse Myers <jesse.myers@gmail.com>
2019-04-22 17:26:06 -0700

    Use t2.micro instances for NAT
    Nat Gateways cost $1/day and we need two.

---
+++ modules/network/nat.tf
@@ -0,0 +1,166 @@
+/* AWS NatGatway costs about $1/day per instance. We should be able to do better using a t2.micro instance.
+ *
+ * See: https://github.com/terraform-community-modules/tf_aws_nat/blob/master/main.tf
+ */
+resource "aws_security_group" "nat" {
+  name        = "nat"
+  description = "Allow nat traffic"
+  vpc_id      = "${aws_vpc.vpc.id}"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+data "aws_iam_policy_document" "nat" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "ec2:ReplaceRoute",
+      "ec2:CreateRoute",
+      "ec2:DeleteRoute",
+      "ec2:DescribeRouteTables",
+      "ec2:DescribeNetworkInterfaces",
+      "ec2:DescribeInstanceAttribute",
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "sts:AssumeRole",
+    ]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+  }
+}
+
+data "aws_region" "current" {}
+
+resource "aws_iam_instance_profile" "nat" {
+  name = "${var.name}.nat"
+  role = "${aws_iam_role.nat.name}"
+}
+
+resource "aws_iam_role" "nat" {
+  name               = "${var.name}.nat"
+  path               = "/"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role.json}"
+}
+
+resource "aws_iam_role_policy" "nat" {
+  name   = "${var.name}.nat"
+  role   = "${aws_iam_role.nat.id}"
+  policy = "${data.aws_iam_policy_document.nat.json}"
+}
+
+data "aws_ami" "ami" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["${var.ami_name_pattern}"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["${var.ami_publisher}"]
+}
+
+data "template_file" "user_data" {
+  count = "${local.min_az_count}"
+
+  template = "${file("${path.module}/nat.conf.tmpl")}"
+
+  vars {
+    name              = "${var.name}"
+    mysubnet          = "${element(aws_subnet.private.*.id, count.index)}"
+    vpc_cidr          = "${aws_vpc.vpc.cidr_block}"
+    region            = "${data.aws_region.current.name}"
+    awsnycast_deb_url = "https://github.com/bobtfish/AWSnycast/releases/download/v0.1.5/awsnycast_0.1.5-425_amd64.deb"
+    identifier        = "rt-private"
+  }
+}
+
+resource "aws_instance" "nat" {
+  count = "${local.min_az_count}"
+
+  ami                    = "${data.aws_ami.ami.id}"
+  instance_type          = "t2.nano"
+  key_name               = "jesse"
+  source_dest_check      = false
+  iam_instance_profile   = "${aws_iam_instance_profile.nat.id}"
+  subnet_id              = "${element(aws_subnet.public.*.id, count.index)}"
+  vpc_security_group_ids = ["${aws_security_group.nat.id}"]
+  user_data              = "${element(data.template_file.user_data.*.rendered, count.index)}"
+
+  tags {
+    Name = "${var.name}.nat"
+  }
+}
+
+/*
+resource "aws_eip" "nat" {
+  count = "${local.min_az_count}"
+  vpc   = true
+
+  depends_on = [
+    "aws_internet_gateway.public",
+  ]
+}
+
+resource "aws_nat_gateway" "nat" {
+  count         = "${local.min_az_count}"
+  subnet_id     = "${element(aws_subnet.public.*.id, count.index)}"
+  allocation_id = "${element(aws_eip.nat.*.id, count.index)}"
+
+  tags {
+    Name = "${var.name}"
+  }
+}
+*/
+
+resource "aws_route_table" "private" {
+  count  = "${local.min_az_count}"
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    instance_id = "${element(aws_instance.nat.*.id, count.index)}"
+    // nat_gateway_id = "${element(aws_nat_gateway.nat.*.id, count.index)}"
+  }
+
+  tags {
+    Name = "${var.name}"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  count          = "${local.min_az_count}"
+  subnet_id      = "${element(aws_subnet.private.*.id, count.index)}"
+  route_table_id = "${element(aws_route_table.private.*.id, count.index)}"
+}
--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -1,3 +1,11 @@
+variable "ami_name_pattern" {
+  default = "ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"
+}
+
+variable "ami_publisher" {
+  default = "099720109477" # Canonical
+}
+
 variable "cidr_block" {
   default = "10.10.0.0/16"
 }
