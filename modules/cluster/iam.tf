5326871ed6a1e23dca3441939bde5e5a9dff7c5e
Jesse Myers <jesse.myers@gmail.com>
2019-04-23 12:47:08 -0700

    Allow secrets to be accessed by ECS

--- modules/cluster/iam.tf
+++ modules/cluster/iam.tf
@@ -22,3 +22,29 @@ resource "aws_iam_role_policy_attachment" "ecs" {
   role       = "${aws_iam_role.ecs.name}"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
+resource "aws_iam_policy" "secrets" {
+  name = "${var.name}.secrets.ecs"
+  policy = "${data.aws_iam_policy_document.secrets.json}"
+}
+
+resource "aws_iam_role_policy_attachment" "secrets" {
+  role       = "${aws_iam_role.ecs.name}"
+  policy_arn = "${aws_iam_policy.secrets.arn}"
+}
+
+data "aws_iam_policy_document" "secrets" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "secretsmanager:Describe*",
+      "secretsmanager:Get*",
+      "secretsmanager:List*",
+    ]
+
+    resources = [
+      "*"
+    ]
+  }
+}
