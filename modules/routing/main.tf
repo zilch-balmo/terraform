1dbee7a52bb57689b43853a84a20d7d72e7541c1
Jesse Myers <jesse.myers@gmail.com>
2019-04-24 13:58:23 -0700

    Simplify CF

--- modules/routing/main.tf
+++ modules/routing/main.tf
@@ -8,7 +8,14 @@ resource "aws_route53_zone" "root" {
 
 resource "aws_s3_bucket" "root" {
   bucket = "zilch.me"
-  acl    = "public-read"
+
+  /* We cannot use an original access identity in the bucket policy at the same time
+     * as a CloudFront custom original policy. This means we either need a public bucket
+     * (which is fine -- it's empty) or some other bucket policy that uses a shared secret.
+     *
+     * See:  https://github.com/riboseinc/terraform-aws-s3-cloudfront-redirect
+     */
+  acl = "public-read"
 
   website {
     redirect_all_requests_to = "app.zilch.me"
@@ -17,19 +24,30 @@ resource "aws_s3_bucket" "root" {
 
 resource "aws_cloudfront_distribution" "root" {
   origin {
-    domain_name = "${aws_s3_bucket.root.bucket_regional_domain_name}"
+    domain_name = "${aws_s3_bucket.root.website_endpoint}"
     origin_id   = "${local.s3_origin_id}"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_read_timeout    = 30
+
+      origin_ssl_protocols = [
+        "TLSv1.2",
+      ]
+    }
   }
 
   aliases = ["zilch.me"]
 
-  enabled             = true
-  is_ipv6_enabled     = true
-  default_root_object = "index.html"
+  enabled         = true
+  is_ipv6_enabled = true
 
   default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
     cached_methods   = ["GET", "HEAD"]
+    compress         = true
     target_origin_id = "${local.s3_origin_id}"
 
     forwarded_values {
